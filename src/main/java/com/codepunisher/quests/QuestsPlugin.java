2c9b13d5aba865e88781733a7d19dba6b323ba18
TrevorMickelson <social@trevmick.com>
2024-01-16 15:55:19 -0600

    first commit

---
+++ src/main/java/com/codepunisher/quests/QuestsPlugin.java
@@ -0,0 +1,9 @@
+package com.codepunisher.quests;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class QuestsPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+}
